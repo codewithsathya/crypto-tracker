d206de0a9e9294fcd3d5d96203acc7c25dc633b9
S Sathiya <codewithsathya@gmail.com>
2021-08-02 11:16:18 +0530

    Fixed sorting

--- src/utils/sortData.js
+++ src/utils/sortData.js
@@ -1,6 +1,14 @@
 import _ from 'lodash';
 
 const sortData = (data, sortColumn) => {
+  if(sortColumn.path === "pair"){
+    return data.sort((a, b) => {
+      let pairA = a.pair.props.children[0].props.children + a.pair.props.children[2];
+      let pairB = b.pair.props.children[0].props.children + b.pair.props.children[2];
+      return sortColumn.order==="asc"? pairA.localeCompare(pairB) : pairB.localeCompare(pairA);
+    })
+  }
+
   if (data.length !== 0 && typeof data[0][sortColumn.path] === "number") {
     if (sortColumn.order === "asc") {
       return data.sort((a, b) => a[sortColumn.path] - b[sortColumn.path]);
